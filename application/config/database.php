fcac5473edc5487f19557b347b533d157fa25d90
Fadilah Riczky <friczky@gmail.com>
2021-02-02 06:47:25 +0700

    update config

--- application/config/database.php
+++ application/config/database.php
@@ -25,3 +25,26 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'ppm_spaceid',
+// 	'password' => 'tFd5CxN6M8Ds4fKt',
+// 	'database' => 'ppm_spaceid',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
